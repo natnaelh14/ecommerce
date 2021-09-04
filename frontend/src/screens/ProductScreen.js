3be6a2b96e2bf58ac3c3b9e22ef27a9db2c15035
natnaelh14 <natnaelh14@github.com>
2021-09-04 18:12:40 -0400

    adding enviromental variables

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -27,7 +27,7 @@ const ProductScreen = ({ match }) => {
 
   return (
     <>
-      <Link className='btn btn-light my-3'>Go Back</Link>
+      <Link to='/' className='btn btn-light my-3'>Go Back</Link>
       <Row>
         <Col md={6}>
           <Image src={product.image} alt={product.name} fluid />
