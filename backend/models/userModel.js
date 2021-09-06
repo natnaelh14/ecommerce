6ee858fbb95628541b8e23c9a01b4c49d5bb979d
natnaelh14 <natnaelh14@github.com>
2021-09-05 23:59:04 -0400

    created userschema model

---
+++ backend/models/userModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+//instead of doing 'created at' and 'updated at', you can pass an option 'timestamps'.
+}, {
+    timestamps: true
+})
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
