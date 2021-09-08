7364dbae68515ecfeec3febf823086dd0b937e27
natnaelh14 <natnaelh14@github.com>
2021-09-08 12:12:55 -0400

    created custom error handling

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -25,7 +25,9 @@ router.get(
     if (product) {
       res.json(product);
     } else {
-      res.status(404).json({ message: 'Product not found' });
+      //if you don't specify status, it will default to 500.
+      res.status(404)
+      throw new Error('Product not found.')
     }
   })
 );
