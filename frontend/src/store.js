31be306e00383dd572b63763d46d066215945eb8
natnaelh14 <natnaelh14@github.com>
2021-09-09 07:37:06 -0400

    create a redux store

---
+++ frontend/src/store.js
@@ -0,0 +1,18 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const reducer = combineReducers({});
+
+//if we want something to be loaded, when redux store initially loads, we put it in here.
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
