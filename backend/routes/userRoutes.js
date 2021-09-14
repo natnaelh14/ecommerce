c0d62e8d203dc6747042e028cf2c3a4c9171bce1
natnaelh14 <natnaelh14@github.com>
2021-09-14 12:08:52 -0400

    created user registration

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const router = express.Router();
-import { authUser, getUserProfile } from '../controllers/userControllers.js';
+import { authUser, registerUser, getUserProfile } from '../controllers/userControllers.js';
 import { protect } from '../middleware/authMiddleware.js';
 
+router.route('/').post(registerUser)
 router.post('/login', authUser );
 // To implement middleware, we put 'protect' as the first argument.
 router.route('/profile').get(protect, getUserProfile)
