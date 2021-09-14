7fe182976669e8c63e48203240e74d33783a1ec0
natnaelh14 <natnaelh14@github.com>
2021-09-14 10:35:16 -0400

    created protect authmiddleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 const router = express.Router();
 import { authUser, getUserProfile } from '../controllers/userControllers.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 router.post('/login', authUser );
-router.route('/profile').get(getUserProfile)
+// To implement middleware, we put 'protect' as the first argument.
+router.route('/profile').get(protect, getUserProfile)
 
 export default router;
