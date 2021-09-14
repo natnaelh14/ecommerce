5d22304d00309ff034403e180b162ef8db08ec19
natnaelh14 <natnaelh14@github.com>
2021-09-14 05:44:15 -0400

    created user profile route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 const router = express.Router();
-import { authUser } from '../controllers/userControllers.js';
+import { authUser, getUserProfile } from '../controllers/userControllers.js';
 
-router.post('/login', authUser )
+router.post('/login', authUser );
+router.route('/profile').get(getUserProfile)
 
 export default router;
