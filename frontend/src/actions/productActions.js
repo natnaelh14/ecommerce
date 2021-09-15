a266ecd75f62b0b36625b8a4f997ac0308ee6d98
natnaelh14 <natnaelh14@github.com>
2021-09-15 06:13:50 -0400

    created user login reducer

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -10,33 +10,39 @@ import {
 
 //These are action creators, 'PRODUCT_LIST_REQUEST' are action being dispatched back to reducer.
 export const listProducts = () => async (dispatch) => {
-    try {
-        dispatch({ type: PRODUCT_LIST_REQUEST })
-        const { data } = await axios.get('/api/products')
-        dispatch({ 
-            type: PRODUCT_LIST_SUCCESS,
-            payload: data
-        })
-    } catch (e) {
-        dispatch({ 
-            type: PRODUCT_LIST_FAIL,
-            payload: e.response && e.response.data.message ? e.response.data.message : e.message
-        })
-    }
-}
+  try {
+    dispatch({ type: PRODUCT_LIST_REQUEST });
+    const { data } = await axios.get('/api/products');
+    dispatch({
+      type: PRODUCT_LIST_SUCCESS,
+      payload: data,
+    });
+  } catch (e) {
+    dispatch({
+      type: PRODUCT_LIST_FAIL,
+      payload:
+        e.response && e.response.data.message
+          ? e.response.data.message
+          : e.message,
+    });
+  }
+};
 
 export const listProductDetails = (id) => async (dispatch) => {
-    try {
-        dispatch({ type: PRODUCT_DETAILS_REQUEST })
-        const { data } = await axios.get(`/api/products/${id}`)
-        dispatch({ 
-            type: PRODUCT_DETAILS_SUCCESS,
-            payload: data
-        })
-    } catch (e) {
-        dispatch({ 
-            type: PRODUCT_DETAILS_FAIL,
-            payload: e.response && e.response.data.message ? e.response.data.message : e.message
-        })
-    }
-}
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+    const { data } = await axios.get(`/api/products/${id}`);
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (e) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload:
+        e.response && e.response.data.message
+          ? e.response.data.message
+          : e.message,
+    });
+  }
+};
