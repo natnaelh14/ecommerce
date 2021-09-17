5811246b9a6f0276f4ace00e56ecc360601397f4
natnaelh14 <natnaelh14@github.com>
2021-09-17 14:43:47 -0400

    enabled update user profile and fixed navbar bug

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -124,31 +124,39 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
   try {
     dispatch({
       type: USER_UPDATE_PROFILE_REQUEST,
-    });
-    //destructuring to retrieve access the token
-    const { userLogin: { userInfo }} = getState();
+    })
+    //Destructuring to access the token.
+    const {
+      userLogin: { userInfo },
+    } = getState()
 
     const config = {
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${userInfo.token}`
+        Authorization: `Bearer ${userInfo.token}`,
       },
-    };
-    const { data } = await axios.put(
-      `/api/users/profile`, user,
-      config
-    );
+    }
+    const { data } = await axios.put(`/api/users/profile`, user, config)
     dispatch({
       type: USER_UPDATE_PROFILE_SUCCESS,
       payload: data,
-    });
-  } catch (e) {
+    })
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    })
+    localStorage.setItem('userInfo', JSON.stringify(data))
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message
+    if (message === 'Not authorized, token failed') {
+      dispatch(logout())
+    }
     dispatch({
       type: USER_UPDATE_PROFILE_FAIL,
-      payload:
-        e.response && e.response.data.message
-          ? e.response.data.message
-          : e.message,
-    });
+      payload: message,
+    })
   }
-};
+}
