9749dc81077bf25f406c49823e33969a595a26a9
natnaelh14 <natnaelh14@github.com>
2021-09-30 19:20:18 -0400

    created paypal script for useeffect

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,11 @@ app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes)
 
+//PayPal client id is stored in a the .env file.
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+)
+
 app.use(notFound)
 
 app.use(errorHandler)
