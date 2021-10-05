9879b47fd0549fc6bf89cdff0f738893902e1ca1
natnaelh14 <natnaelh14@github.com>
2021-10-05 15:21:22 -0400

    fixed error with paypal

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-
+import { GrLinkedinOption, GrGithub, GrMail } from 'react-icons/gr';
 
 const Footer = () => {
-    return (
-        <footer>
-            <Container>
-                <Row>
-                    <Col className= 'text-center py-3'>Copyright &copy; SpiceLand</Col>
-                </Row>
-            </Container>
-        </footer>
-    )
-}
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>Copyright &copy; SpiceLand</Col>
+        </Row>
+        <Row>
+          <Col className='text-center' md="2" >
+            {' '}
+            <a
+              href='https://www.linkedin.com/in/natnael-haile-b146b41bb/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <GrLinkedinOption />
+            </a>
+          </Col>
+          <Col className='text-center' md="2">
+            {' '}
+            <a
+              href='https://github.com/natnaelh14'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <GrGithub />
+            </a>
+          </Col>
+          <Col className='text-center' md="2">
+            {' '}
+            <a
+              href='mailto:haile.natnael@natnaeldev.com'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <GrMail />
+            </a>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
