c331baab3051faf5bd90c3cf478337d99b298a74
natnaelh14 <natnaelh14@github.com>
2021-10-05 19:23:22 -0400

    update heroku

--- backend/server.js
+++ backend/server.js
@@ -19,11 +19,17 @@ app.use(express.urlencoded({ extended: false }));
 //it allows us to accept JSON in the body
 app.use(express.json());
 
+
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
 }
 
 app.use('/api/products', productRoutes);
