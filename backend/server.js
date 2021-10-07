9c00a7021f4c19c6adf47b61b7e754d30ccf0e29
natnaelh14 <natnaelh14@github.com>
2021-10-07 17:26:42 -0400

    heroku update

--- backend/server.js
+++ backend/server.js
@@ -21,14 +21,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
-
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
 }
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
+
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
