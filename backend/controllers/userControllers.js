e24f71da553947a7fde58505471a3602e834151a
natnaelh14 <natnaelh14@github.com>
2021-10-08 18:08:16 -0400

    get user by id and update user endpoints

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -147,7 +147,7 @@ const getUserById = asyncHandler(async (req, res) => {
 // @route - PUT /api/users/:id
 // @access - Private/Admin
 const updateUser = asyncHandler(async (req, res) => {
-  const user = await User.findById(req,params.id);
+  const user = await User.findById(req.params.id);
   if (user) {
     user.name = req.body.name || user.name;
     user.email = req.body.email || user.email;
