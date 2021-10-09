35c0841b0d86d982b6f3c97c1c79079db61ccab8
natnaelh14 <natnaelh14@github.com>
2021-10-09 15:19:46 -0400

    create and update product endpoints

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -59,21 +59,6 @@ const createProduct = asyncHandler(async (req, res) => {
   res.status(201).json(createdProduct);
 });
 
-// @desc - Delete a product
-// @route - DELETE /api/products/:id
-// @access - Private/Admin
-const deleteProduct = asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id);
-  if (product) {
-    await product.remove();
-    res.json({ message: 'Product Removed' });
-  } else {
-    // if you don't specify status, it will default to 500.
-    res.status(404);
-    throw new Error('Product not found.');
-  }
-});
-
 // @desc - Update a product
 // @route - PUT /api/products/:id
 // @access - Private/Admin
