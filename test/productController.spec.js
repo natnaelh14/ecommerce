df9310b61d4d18640d5f7ae57273867b0916aaf8
natnaelh14 <natnaelh14@github.com>
2021-10-09 19:59:23 -0400

    test backend

---
+++ test/productController.spec.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+import mongoose from 'mongoose';
+import mocha from 'mocha';
+import chai from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
+const { expect } = chai;
+chai.use(sinonChai);
+
+// DESCRIBE block
+describe('', () => {
+  // IT block
+  it('', () => {
+    // Your test goes here
+  });
+});
+/* eslint-disable */
