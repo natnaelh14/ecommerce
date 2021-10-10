31c96a3e0327f161bd2fbfc12ee6f31f0390ad42
natnaelh14 <natnaelh14@github.com>
2021-10-10 13:39:01 -0400

    created product update reducer

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -12,6 +12,10 @@ import {
   PRODUCT_CREATE_SUCCESS,
   PRODUCT_CREATE_FAIL,
   PRODUCT_CREATE_RESET,
+  PRODUCT_UPDATE_REQUEST,
+  PRODUCT_UPDATE_SUCCESS,
+  PRODUCT_UPDATE_FAIL,
+  PRODUCT_UPDATE_RESET,
 } from '../constants/productConstants';
 
 // State for product list, which we will see in the homepage
@@ -81,6 +85,24 @@ export const productCreateReducer = (
       return { loading: false, error: action.payload };
     case PRODUCT_CREATE_RESET:
       return {};
+    default:
+      return state;
+  }
+};
+
+export const productUpdateReducer = (
+  state = { product: {} },
+  action,
+) => {
+  switch (action.type) {
+    case PRODUCT_UPDATE_REQUEST:
+      return { loading: true };
+    case PRODUCT_UPDATE_SUCCESS:
+      return { loading: false, success: true, product: action.payload };
+    case PRODUCT_UPDATE_FAIL:
+      return { loading: false, error: action.payload };
+    case PRODUCT_UPDATE_RESET:
+      return { product: {} };
     default:
       return state;
   }
