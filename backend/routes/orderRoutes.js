46028841e87d46508ac536458a46719170e06df6
natnaelh14 <natnaelh14@github.com>
2021-10-10 15:48:25 -0400

    added updateordertodelivered to router

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -6,6 +6,7 @@ import {
   getOrders,
   getOrderById,
   updateOrderToPaid,
+  updateOrderToDelivered
 } from "../controllers/orderController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 /* eslint-disable */
@@ -16,5 +17,6 @@ router.route("/").post(protect, addOrderItems).get(protect, admin, getOrders);
 router.route("/myorders").get(protect, getMyOrders);
 router.route("/:id").get(protect, getOrderById);
 router.route("/:id/pay").put(protect, updateOrderToPaid);
+router.route("/:id/pay").put(protect, admin, updateOrderToDelivered);
 
 export default router;
