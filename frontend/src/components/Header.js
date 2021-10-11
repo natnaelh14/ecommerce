6d58684612a2ec5b8f012931a1f43c4f7dbd9f1c
natnaelh14 <natnaelh14@github.com>
2021-10-11 19:36:30 -0400

    added search box into header component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { LinkContainer } from 'react-router-bootstrap';
 import {
@@ -6,6 +7,7 @@ import {
 } from 'react-bootstrap';
 import { logout } from '../actions/userActions';
 import LogoImage from './img/logo.png';
+import SearchBox from './SearchBox';
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -35,6 +37,7 @@ const Header = () => {
           <Navbar.Brand>SpiceLand & Co.</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
+            <Route render={({ history }) => <SearchBox history={history} />} />
             <Nav className="ms-auto">
               <LinkContainer to="/cart">
                 <Nav.Link>
--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -1,28 +1,28 @@
-import React, { useState } from "react";
-import { Form, Button } from "react-bootstrap";
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
 
 const SearchBox = ({ history }) => {
-  const [keyword, setKeyword] = useState("");
+  const [keyword, setKeyword] = useState('');
 
   const submitHandler = (e) => {
     e.preventDefault();
     if (keyword.trim()) {
       history.push(`/search/${keyword}`);
     } else {
-      history.push("/");
+      history.push('/');
     }
   };
 
   return (
     <Form onSubmit={submitHandler} inline>
-      <Form.control
+      <Form.Control
         type="text"
         name="q"
         onChange={(e) => setKeyword(e.target.value)}
         placeholder="Search Products..."
         className="mr-sm-2 ml-sm-5"
-      ></Form.control>
-      <Button type="submit" variant="outline success" className="p-2">
+      />
+      <Button type="submit" variant="outline-success" className="p-2">
         Search
       </Button>
     </Form>
