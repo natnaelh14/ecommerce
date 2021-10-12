a8864f8f6593e081b641afec88949c9d2a7d22f4
natnaelh14 <natnaelh14@github.com>
2021-10-11 22:35:00 -0400

    server.js update

--- backend/server.js
+++ backend/server.js
@@ -46,15 +46,15 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')));
-  app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html')));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
 } else {
+  // If we are not in production
   app.get('/', (req, res) => {
     res.send('API is running....');
   });
 }
 
 app.use(notFound);
-
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
